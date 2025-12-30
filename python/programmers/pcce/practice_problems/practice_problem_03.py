5a81921ae894c07b9b38fe397777b828fb15e0bb
LEEJAEWON <leejaewon264@gmail.com>
2025-12-30 09:50:12 +0900

    pcce대비 실습문제 01-03 풀이

--- python/programmers/pcce/practice_problems/practice_problem_03.py
+++ python/programmers/pcce/practice_problems/practice_problem_03.py
@@ -36,4 +36,11 @@ def solution(num1, num2):
 # 풀이 코드
 def solution(num1, num2):
     answer = 0
+    
+    if num1 == num2:
+        answer = 1
+    
+    else:
+        answer = -1
+    
     return answer
