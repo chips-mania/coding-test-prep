b2dee064c0c90f42e309cbb87f865bcf10bdd375
Lee Jaewon <leejaewon264@gmail.com>
2026-02-24 16:25:44 +0900

    진법변환2 ; 10진법을 특정 진법으로 변환 ; 0~Z까지 담긴 리스트의 인덱스를 활용 ; 진법 수로 계속 나누는 것이 포인트

---
+++ python/baekjoon/단계별로풀어보기/수학1/11005.py
@@ -0,0 +1,21 @@
+#### 진법변환 2
+
+N, B = map(int,input().split())
+
+# 인덱스활용
+digit = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+# B진법으로 변환한 답
+ans = ""
+
+# N에서 계속 나누어야하므로
+while N > 0:
+    # ans 갱신
+    # 1의 자리의 수 부터 왼족에 추가됨
+    ans = digit[N%B] + ans
+
+    # N 갱신
+    N //= B
+
+    # print(ans)
+print(ans)
